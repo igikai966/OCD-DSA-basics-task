f7fc1dd82a4126478a01cbf10f46a2749dca2d1e
Lavya <lavya075@gmail.com>
2025-03-18 09:13:58 +0000

    chn

--- Day12/ques2.cpp
+++ Day12/ques2.cpp
@@ -1,2 +1,32 @@
 //Question 2- Find first and last position of element in sorted array
 //link-https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+class Solution {
+    public:
+    
+       int binarySearch(vector<int>& nums, int target, int l,int r,int isFirstOccur){
+        int idx=-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(nums[m] == target){
+                idx=m;
+                if(isFirstOccur){
+                    r=m-1;
+                }
+                else l=m+1;
+            }
+            
+        }
+        return idx;
+    
+       }
+    
+    
+    
+        vector<int> searchRange(vector<int>& nums, int target) {
+            int firstindex = binarySearch( nums,target, 0,nums.size()-1,1);
+            int lastindex= binarySearch ( nums,target,0,nums.size()-1,0);
+            return{firstindex, lastindex};
+            
+        }
+    
+    };
--- Day12/ques3.cpp
+++ Day12/ques3.cpp
@@ -1,2 +1,20 @@
 //Question 3- Search Insert Position
 //link-https://leetcode.com/problems/search-insert-position/description/
+class Solution {
+    public:
+        
+        long long int searchInsert(vector<int>& nums, int target) {
+            
+             long long int s=0,e=nums.size()-1;
+             //base case
+             if(nums[e]<target)return e+1;
+            while(s<=e){
+                long long int m=(s+e)/2;
+                if(nums[m]==target)return m;
+                if(nums[m]<target)s=m+1;
+                else e=m-1;
+            }
+            return s;
+            
+        }
+    };
