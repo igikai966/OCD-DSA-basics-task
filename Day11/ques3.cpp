2eba429b912684816ddcf3e88e7c3510875729f6
Lavya <lavya075@gmail.com>
2025-03-18 07:52:38 +0000

    change3

--- Day11/ques3.cpp
+++ Day11/ques3.cpp
@@ -1,2 +1,37 @@
 //Question 3-Spiral Matrix
 //link- https://leetcode.com/problems/spiral-matrix/description/
+class Solution {
+    public:
+        vector<int> spiralOrder(vector<vector<int>>& matrix) {
+            int rowstart=0,colstart=0;
+            int n=matrix.size(); //number of rows
+            int m=matrix[0].size(); // number of columns
+            int rowend=n,colend=m;
+            vector<int>ans(n*m);
+            while(rowstart<=rowend && colstart<=colend)
+            {
+                for(int j=colstart;j<colend;j++){
+                    ans.push_back(matrix[rowstart][j]);
+                }
+                rowstart++;
+                for(int j=rowstart;j<rowend;j++){
+                    ans.push_back(matrix[j][colend]);
+                }
+                colend--;
+                if(rowstart<=rowend){
+                for(int j=colend;j>=colstart;j--){
+                    ans.push_back(matrix[rowend][j]);
+                }
+                rowend--;
+                }
+                if(colstart<=colend){
+                for(int j=rowend;j>=rowstart;j--){
+                    ans.push_back(matrix[j][colstart]);
+    
+                }
+                colstart++;
+                }
+            }
+            return ans;
+        }
+    };
