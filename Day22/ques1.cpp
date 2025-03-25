e8533263e12d85cbba1fd1bbad84faf533edf8e6
Lavya <lavya075@gmail.com>
2025-03-25 23:22:27 +0000

    change

--- Day22/ques1.cpp
+++ Day22/ques1.cpp
@@ -1,2 +1,18 @@
 //Question 1- Minimum Add to make Parenthesis Valid
 //link-https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+//code-
+class Solution {
+    public:
+        int minAddToMakeValid(string s) {
+            int open=0,close=0;
+            for(auto c:s){
+                if(c =='(')open++;
+                else{
+                    if(open>0)open--;
+                    else close++;
+                }
+            }
+            return open+close;
+            
+        }
+    };
